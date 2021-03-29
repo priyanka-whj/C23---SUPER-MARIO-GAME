dcb050e1055581947ea960e26206d051bc62a3e5
priyanka-whj <79097513+priyanka-whj@users.noreply.github.com>
2021-03-29 13:40:08 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,30 @@
+class Player
+{
+    constructor()
+    {
+        this.spt = createSprite(200, 200, 100, 100);
+        this.spt.shapeColor = "blue";
+        this.spt.addAnimation("mario", marioAnimation);
+        this.spt.scale = 0.02;
+    }
+
+    applyGravity()
+    {
+        this.spt.velocityY = this.spt.velocityY + 2;
+    }
+
+    moveForward()
+    {
+        this.spt.x = this.spt.x + 10;
+    }
+
+    moveBackward()
+    {
+        this.spt.x = this.spt.x - 10;
+    }
+
+    jump()
+    {
+        this.spt.velocityY = -20;
+    }
+}
